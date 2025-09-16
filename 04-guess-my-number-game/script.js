6cdc0575241e46ed1514b2d81238a4cfe66175a0
Laurence Ser <82258782+SerrLrnc@users.noreply.github.com>
2025-09-16 22:13:43 +0800

    Add files via upload

---
+++ 04-guess-my-number-game/script.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const MIN_NUMBER = 1;
+const MAX_NUMBER = 20;
+const START_SCORE = 20
+
+const bodyEl = document.body
+const messageEl = document.querySelector(`.message`);
+const scoreEl = document.querySelector(`score`);
+const numberEl = document.querySelector('.number');
+const highscoreEl = document.querySelector('.highscore');
+const guessEl = document.querySelector('.guess');
+const checkBtnEl = document.querySelector('.check');
+const againBtnEl = document.querySelector('.again');
+
+//01 Helpers
+function setMessage (text){
+    messageEl.textContent = text;
+}
+function secretNumber(value){
+    numberEl.textContent = value;
+}
+function setScore(value) {
+    scoreEl.textContent = value;
+}
+function setHighcore(value){
+    highscoreEl.textContent = value;
+}
+function setBackground(color) {
+    body.El.style.backgroundColor = color;
+}
+function disablePlay(disabled) {
+    guessInputEl.disabled = disabled;
+    checkBtnEl.disabled = disabled;
+}
+function clearInput () {
+    guessInputEl.value = '';
+}
+
+//Game State Reset
+
+let secretNUmber = Math.trunc(Math.random() * MAX_NUMBER ) + MIN_NUMBER;
+let score = START_SCORE
+let highscore = 0;
+
+function resetGameState(){
+    score = START_SCORE;
+    secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+}
+
+function renderInitialUI(){
+    setMessage('Start guessing...');
+    setNumber('?');
+    setScore(score);
+    clearInput();
+    disablePlay(false);
+    setBackground('');
+}
+
+//Handlers
+checkBtnEl.addEventListener('click', function ()) {
+    const guess = Number (guessInputEl.value);
+    console.log('Player guessed:'. guess);
+}
+
+
+if (!guess) {
+    document.querySelector('.message').textContent = 'No Number';
+    return;
+}
+
+if (guess < MIN_NUMBER || guess > MAX_NUMBER){
+    return setMessage (`Number must be between ${MIN_NUMBER} and ${MAX_NUMBER}`);
+}
+
+if (guess === secretNumber) {
+    setMessage ('Correct Number!');
+    setNumber(secretNUmber);
+    setBackground('green');
+}
+
+
+document.querySelector('.again').addEventListener('click', function (){
+   resetGameState ();
+   renderInitialUI ();
+});
+
+
