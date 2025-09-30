5991131a670146e398f0f9a921af370103a0bfc9
Laurence Ser <82258782+SerrLrnc@users.noreply.github.com>
2025-10-01 02:39:23 +0800

    Add files via upload

---
+++ 07-behind-the-scenes/script.js
@@ -0,0 +1,167 @@
+// 'use strict';
+
+// console.log(varX);
+// // console.log(letX);
+// // console.log(constX);
+
+// var varX = 1;
+// let letX = 2;
+// const constX = 3;
+
+// function addDecl (a, b) {
+//     return a + b;
+// }
+// const addExpr = function (a, b){
+//     return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+// const person = {
+//     name: 'Jonas',
+//     greet: function () {
+//         console.log(`Hello I am ${this.name}`);
+//     },
+// };
+
+// person.greet();
+
+// const anotherPerson = { name: 'Sarah'};
+// //Borrow greet function
+// anotherPerson.greet = person.greet;
+// //Display or run the greet function for anotherperson
+// anotherPerson.greet(); 
+
+// //Detached function
+// const greetFunction = person.greet;
+
+// greetFunction();
+
+
+// const obj = {
+//     name: 'Objects',
+//     regularMethod: function (){
+//         console.log('Regular:', this.name);
+//     },
+//     arrowMethod: () => {
+//         console.log('Arrow:', this.name);
+//     },
+// };
+
+// obj.regularMethod();
+
+// obj.arrowMethod();
+
+// //Before running, predict the output:
+// const quiz = {
+//     name: 'Quiz Object',
+//     regularMethod() {
+//       console.log('Regular:', this.name);
+//     },
+//     arrowMethod: () => {
+//       console.log('Arrow:', this.name);
+//     },
+//   };
+  
+//   quiz.regularMethod(); // Predict: ?
+//   quiz.arrowMethod(); // Predict: ?
+
+// const time = {
+//     name: 'Timer',
+//     start: function(){
+//     console.log(`${this.name} starting...` );
+    
+//     const self = this;
+
+//     selfTimeout(function (){
+//         console.log(`${self.name} finished`);
+//         }, 1000);
+//     },
+//     startModern:function (){
+//         console.log(`${this.name} starting modern...`);
+
+//         setTimeout(() => {
+//             console.log(`${this.name} finished modern`);
+//         }, 1500);
+//     },
+// };
+
+// timer.start();
+// timer.startModern();
+
+const functionTypes = {
+    regularFunction: function () {
+        console.log('Arguments length:', arguments.length);
+        console.log('First argument:', argument[0]);
+    },
+
+    arrowFunction: () => {
+        console.log(arguments);
+        console.log('Arrow function called');
+    },
+    modernFunction: (...args) => {
+        console.log('Args length', args.length);
+        console.log('First arg:', args[0]);
+    },
+};
+
+functionTypes.regularFunction('hello', 'world');
+functionTypes.arrowFunction('test');
+functionTypes.modernFunction('modern', 'approach');
+
+
+// let age = 30;
+
+// let oldage = age;
+
+// age = 31;
+ 
+// console.log('age', age);
+// console.log('oldAge', oldage);
+
+// const me = { name: 'Aiken', age: 25};
+
+// const friend = me;
+
+// friend.name = 'JohnDoe';
+// friend.age = 23;
+
+
+// consloge.log('me:', me);
+// consloge.log('friend:', friend);
+
+// const original = {
+//     name: 'Alie',
+//     age: 28,
+//     hobbies: ['reading', 'coding'],
+// };
+
+// const shallowCopy = { ...original };
+
+// shallowCopy.name = 'Bob';
+
+// console.log('original name:', original.name)
+// console.log('copy name:', shallowCopy.name);
+
+// shallowCopy.hobbies.push('gaming');
+
+// console.log(original.hobbies);
+// console.log(shallowCopy.hobbies);
+
+// const deepOriginal = {
+//     name: 'Charlie',
+//     age: 32,
+//     //nested object
+//     address: { city: 'Paris', country: 'France'},
+//     //nested array
+//     hobbies ['travel', 'photography'],
+// };
+
+// const deepCopy = structuredClone (deepOriginal);
+
+// deepCopy.address.city = 'London';
+// deepCopy.hobbies.push ('cooking');
+// deepCopy.name =  'Lex';
+
+// console.log(deepOriginal);
+// console.log(deepCopy);
